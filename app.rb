240fb59d5f99286c0468c5f506ce6725dc1eab45
ashley williams <heyashleyashley@gmail.com>
2013-12-22 00:18:02 -0500

    nicer display

--- app.rb
+++ app.rb
@@ -7,6 +7,8 @@ class Game_of_Life < Sinatra::Application
 
   enable :sessions
 
+  configure(:development) { set :session_secret, "something" }
+
   get "/" do
     session[:game] ||= Game.new
     session[:game].play
